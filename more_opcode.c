096d5dc0a208b7454ae9566e64caec301c383991
a-sowa <axelsowapro@gmail.com>
2024-01-03 17:37:37 +0000

    Adding pop function

--- more_opcode.c
+++ more_opcode.c
@@ -16,3 +16,33 @@ void pint(stack_t **stack, unsigned int line_number)
 	}
 	printf("%i\n", (*stack)->n);
 }
+
+/**
+ * pop - Removes the top element of the stack.
+ * @stack: Double pointer to the top of the stack.
+ * @line_number: The current line number in the script file.
+ * Return: void
+ */
+
+void pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = *stack;
+
+	if (!*stack || !stack)
+	{
+		dprintf(STDERR_FILENO, "L%i: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((*stack)->next != NULL)
+	{
+		*stack = temp->next;
+		(*stack)->prev = NULL;
+		free(temp);
+	}
+	else
+	{
+		free(*stack);
+		*stack = NULL;
+	}
+}
